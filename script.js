d92a79431bd2cacf6af58e3606e5e30f46356ab7
nv23087-ali-abdulmahdi <NV23087@nvtc.edu.bh>
2025-03-16 09:45:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function add7 (a){
+    return a + 7
+}
+console.log(add7(11))
+
+function multiply (a,b){
+    return a * b
+}
+console.log(multiply(2,2))
+
+function captilaize (str) {
+    return str.at(0).toUpperCase() + str.slice(1)
+
+}
+console.log(captilaize("ali"))
+function lastLetter(str1) {
+    return str1.charAt(str1.length - 1);
+}
+
+console.log(lastLetter("ali")); 
