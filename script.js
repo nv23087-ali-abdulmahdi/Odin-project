d9cbecf01716e322e5f1cf16d6876096af3ab0e9
nv23087-ali-abdulmahdi <NV23087@nvtc.edu.bh>
2025-03-18 10:14:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -26,14 +26,48 @@ let computerScore=0;
 //step 5 play single game
 
 function playRound(humanChoice, computerChoice) {
- if humanChoice
+  humanChoice = humanChoice.toLowerCase();
+  if (humanChoice === computerChoice) {
+    console.log(`NO WINNER TIE ${humanSelection} is the same as ${computerSelection}`);
+  } else if (
+    (humanChoice == "rock" && computerChoice == "scissors") ||
+    (humanChoice == "paper" && computerChoice == "rock") ||
+    (humanChoice == "scissors" && computerChoice == "paper")
+  ) {
+    console.log(`human win ${humanSelection} beats ${computerSelection}`);
+    humanScore++;
+  } else {
+    console.log(`AI WINS ${computerChoice} beats ${humanSelection}`);
+    computerScore++;
+  }
+
+
 }
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
 
-    
-
+//step 6 play entire game
+function playGame() {
+  
+    for (let round = 1; round <= 5; round++) {
+      console.log(`Round=${round}`);
+  
+      const humanSelection = getHumanChoice();
+      const computerSelection = getComputerChoice();
+  
+      playRound(humanSelection, computerSelection);
+    }
+  //step 7 check for the winner
+    if (humanScore > computerScore) {
+      console.log(`Congratulations! You win the game!human:${humanScore} computer:${computerScore}`);
+    } else if (humanScore < computerScore) {
+      console.log(`Sorry, you lose the game. Better luck next time! human:${humanScore} computer:${computerScore}`);
+    } else {
+      console.log(`The game is a tie! human:${humanScore} computer:${computerScore}`);
+    }
+  }
+  
+  playGame();
 
